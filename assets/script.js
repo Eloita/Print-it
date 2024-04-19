8f56232b8b5fd30a60f708d67a1536c83a961051
Eloïta <itabyelo@gmail.com>
2024-04-19 11:38:00 +0200

    bug about first white bullet that did not appear

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Tableau des slides à parcourir
 const slides = [
   {
     image: "slide1.jpg",
@@ -17,12 +18,12 @@ const slides = [
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
-//Variables
+//Variables---------------------------------------
 
-//Récupérer le seleteur arrow_left
+//Récupérer le seleteur .arrow_left
 const arrowleft = document.querySelector(".arrow_left");
 
-//Récupérer le seleteur arrow_right
+//Récupérer le seleteur .arrow_right
 const arrowright = document.querySelector(".arrow_right");
 
 let currentIndex = 0;
@@ -36,76 +37,75 @@ let bannerImg = bannerDiv.querySelector(".banner-img");
 // Récupérer la balise p à l'intérieur de la div
 let bannerText = bannerDiv.querySelector("p");
 
+// Récupérer le point rempli du selecteur .dots
+let dotsBullet = document.querySelector(".dots");
+
+//Point correspondant à la longeur du tableau étape 3
+//let i = 0;
+function addDots() {
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("div");
+    dot.className = "dot";
+
+    if (currentIndex === i) {
+      dot.classList.add("dot_selected");
+    }
+    dotsBullet.appendChild(dot);
+  }
+}
+addDots()
+let dotsCheck = document.querySelectorAll(".dot");
 
 //----------------------------------------------
+
 /*event listener sur chacune des flèches*/
 
-// arrow left
+// arrow left ------------------------------------
 arrowleft.addEventListener("click", slideLeft);
 function slideLeft() {
-  // console.log("Vous avez cliqué sur la flèche de gauche");
-  // Supprimer la classe ".dot_selected" de tous les points
-  dotsCheck[i].classList.remove("dot_selected");
+  // console.log("Vous avez cliqué sur la flèche de gauche"); étape 2
 
-  /*i -= 1; lors de l'étape 4, puis condition pour avoir le défilement étape 5*/
-  if (i <= 0) {
-    i = slides.length - 1;
-  } else {
-    i--;
-  }
+  // Supprimer la classe ".dot_selected" de tous les points
+  dotsCheck[currentIndex].classList.remove("dot_selected");
+  /*i -= 1; Renvoyer à la fin du tableau
+    if (i <= 0) {
+      i = slides.length - 1;
+    } else {
+      i--;
+    }*/
+
+  // Mettre à jour l'index de la slide
+  currentIndex = currentIndex === 0 ? slides.length - 1 : currentIndex - 1;
+
   // Ajouter la classe ".dot_selected" au point correspondant à la slide actuelle
-  dotsCheck[i].classList.add("dot_selected");
+  dotsCheck[currentIndex].classList.add("dot_selected");
+
+  // Mettre à jour l'image et le texte du carousel
+  bannerImg.src = `./assets/images/slideshow/${slides[currentIndex].image}`;
+  bannerText.innerHTML = slides[currentIndex].tagLine;
 }
 
-// arrow right
-
+// arrow right -----------------------------------------------------------------------
 arrowright.addEventListener("click", slideRight);
 function slideRight() {
-  /*console.log("Vous avez cliqué sur la flèche de droite");*/
+  //console.log("Vous avez cliqué sur la flèche de droite"); étape 2
+
   // Supprimer la classe ".dot_selected" de tous les points
-  dotsCheck[i].classList.remove("dot_selected");
-  /*i += 1; lors de létape 4, puis condition pour avoir le défilement étape 5*/
+  dotsCheck[currentIndex].classList.remove("dot_selected");
+  /*i += 1; lors de létape 4, puis condition pour avoir le défilement étape 5
 
   if (i >= slides.length - 1) {
     i = 0;
   } else {
     i++;
-  }
+  }*/
+  // Mettre à jour l'index de la slide
+
+  currentIndex = currentIndex === slides.length - 1 ? 0 : currentIndex + 1;
   // Ajouter la classe ".dot_selected" au point correspondant à la slide actuelle
-  dotsCheck[i].classList.add("dot_selected");
-}
+  dotsCheck[currentIndex].classList.add("dot_selected");
 
-let dotsBullet = document.querySelector(".dots");
-
-let i = 0;
-for (let i = 0; i < slides.length; i++) {
-  const dot = document.createElement("div");
-  dot.className = "dot";
-
-  dotsBullet.appendChild(dot);
-}
-
-let dotsCheck = document.querySelectorAll(".dot");
-dotsCheck[i].classList.add("dot_selected");
-
-//--------------------------------
-
-
-
-// arrow left
-arrowleft.addEventListener("click", function() {
-  // Mettre à jour l'index de la slide
-  currentIndex = (currentIndex === 0) ? slides.length - 1 : currentIndex - 1;
   // Mettre à jour l'image et le texte du carousel
   bannerImg.src = `./assets/images/slideshow/${slides[currentIndex].image}`;
   bannerText.innerHTML = slides[currentIndex].tagLine;
-});
-
-// arrow right
-arrowright.addEventListener("click", function() {
-  // Mettre à jour l'index de la slide
-  currentIndex = (currentIndex === slides.length - 1) ? 0 : currentIndex + 1;
-  // Mettre à jour l'image et le texte du carousel
-  bannerImg.src = `./assets/images/slideshow/${slides[currentIndex].image}`;
-  bannerText.innerHTML = slides[currentIndex].tagLine;
-});
+}
